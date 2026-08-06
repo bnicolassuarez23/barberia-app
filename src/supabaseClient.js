fa8982884992d322640e84e18b119c79ef385221
Nico <bnicolas.suarez23@gmail.com>
2026-08-06 20:12:29 -0300

    Fix URL base de Supabase Client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Reemplazá con la URL que copiaste de Supabase (Paso 1.4)
-const supabaseUrl = 'https://twfgzehevxqmlbonfhof.supabase.co/rest/v1/' 
+const supabaseUrl = 'https://twfgzehevxqmlbonfhof.supabase.co' 
 
 // Reemplazá con la clave anon/public que copiaste de Supabase
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3Zmd6ZWhldnhxbWxib25maG9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NzQ2ODQsImV4cCI6MjEwMTQ1MDY4NH0.Ippx84SqXmvUqdzCN45uqZRNdcRKGNRXFL1wYJoqd4U'
